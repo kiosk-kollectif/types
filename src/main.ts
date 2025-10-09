a4489379f79613f4e75d2923642625edaad855b9
faouz-dev <faouzanekouko8@gmail.com>
2025-10-09 17:05:39 +0000

    feat: enable CORS configuration for API

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { OPENAPI_FILE } from './common/utils/constants';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // ⚠️ toutes les origines
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: false,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Kiosk Kollectif API')
     .setDescription('API RestFul complet documenter par swaggie')
