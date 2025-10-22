a2b1f0fef52ba27f36c3640f27d0067425c116e5
Luffy <kevinckahams@gmail.com>
2025-10-22 15:02:01 +0000

    refactor: migrate roles and status enums to types file

---
+++ tools.ts
@@ -0,0 +1,19 @@
+import { UserPublicInfo } from './users.ts';
+
+export enum ToolRequestStatus {
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
+}
+
+export type ToolPublicInfo = {
+  name: string;
+  description: string;
+  thumbnail: string;
+  categories: string[];
+  images: string[];
+  dayprice: number;
+  location?: string;
+  slug: string;
+  owner: UserPublicInfo;
+};
