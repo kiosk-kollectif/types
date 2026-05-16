94adb08102cd979a1ddfe540bb1f5ab974d9359c
faouz-dev <faouzanekouko8@gmail.com>
2026-05-16 10:53:29 +0000

    feat: setup reservation types and postinstall build script

--- src/types/reservations.ts
+++ src/types/reservations.ts
@@ -1,5 +1,19 @@
+import { ToolPublicInfo } from "./tools";
+import { UserPublicInfo } from "./users";
+
 export enum ReservationRequestStatus {
   PENDING = 'en cours',
+  ACCEPTED = 'acceptee',
   COMPLETED = 'terminee',
   CANCELLED = 'annulee',
+}
+
+export interface Reservation {
+  id: string;
+  tool: ToolPublicInfo;
+  renter: UserPublicInfo;
+  start_date: string;
+  end_date: string;
+  status: ReservationRequestStatus;
+  createdAt: string;
 }
