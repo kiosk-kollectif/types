6dabdb3999b4f799dd5711fcafc4e90dfead8c4c
faouz-dev <faouzanekouko8@gmail.com>
2026-06-18 14:36:38 +0000

    feat: ajout de la structure de données pour les demandes de candidats et mise à jour de la méthode de récupération des demandes

--- applicants.ts
+++ applicants.ts
@@ -1,5 +1,17 @@
+import { User } from './users';
+
 export enum ApplicantRequestStatus {
   PENDING = 'pending',
   ACCEPTED = 'accepted',
   REFUSED = 'rejected',
+}
+
+export interface ApplicantRequest {
+  id: string;
+  userId: string;
+  user: User;
+  status: ApplicantRequestStatus;
+  adminNote?: string | null;
+  createdAt: string;
+  updatedAt: string;
 }
