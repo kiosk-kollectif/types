87840bbc79bea9eb14bcec1909c242eff6df2c78
faouz-dev <faouzanekouko8@gmail.com>
2026-06-27 16:28:29 +0000

    feat: ajout du module Warehouses avec gestion des entrepôts, mise à jour des DTO et des services

--- warehouse.ts
+++ warehouse.ts
@@ -1,6 +1,22 @@
-export interface WhareHouse {
+import { User, UserRole } from './users';
+
+export interface WhareHouseManager {
+  userId: string;
+  warehouseId: string;
+  role: UserRole;
+  assignedAt: string;
+  user: User;
+}
+
+export interface WhareHouse extends WhareHousePublicInfo {
+  managers?: WhareHouseManager[];
+}
+
+export interface WhareHousePublicInfo {
   id: string;
   name: string;
   location?: string;
-  capacity?: number;
+  latitude: number;
+  longitude: number;
+  toolsCount?: number;
 }
